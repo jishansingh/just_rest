d42858c0ae271cf73401ffe8f47971ed1bf07a2b
jishansingh <singhjishan2@gmail.com>
2019-05-27 22:54:18 +0545

    use api view in function

--- rest/views.py
+++ rest/views.py
@@ -2,37 +2,36 @@ from django.shortcuts import render
 from .models import NewUser
 from .serializers import UserSerializer
 from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse,JsonResponse
-from rest_framework.parsers import JSONParser
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 # Create your views here.
-@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
 def ListUser(request):
     if request.method=='GET':
         user=NewUser.objects.all()
         serializer=UserSerializer(user,many=True)
-        return JsonResponse(serializer.data,safe=False)
+        return Response(serializer.data)
     elif request.method=='POST':
-        data=JSONParser(request)
-        serialize=UserSerializer(data=data)
+        serialize=UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data,staus=201)
+            return Response(serializer.data,staus=201)
         else:
-            return JsonResponse(serializer.errors,status=400)
-
+            return Response(serializers.errors,status=400)
+@api_view(['GET', 'PUT', 'DELETE'])
 def DetailView(request,id):
     user=get_object_or_404(NewUser,id=id)
     if request.method=='GET':
         serializer=UserSerializer(user)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
     elif request.method=='PUT':
-        data=JsonParser(request)
-        serializer=UserSerializer(user,data=data)
+        serializer=UserSerializer(user,data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
+            return Response(serializer.data)
         else:
-            return JsonResoponse(serializers.errors,status=400)
+            return Response(serializers.errors,status=400)
 
 
